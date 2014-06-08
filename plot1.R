9a404210c40c4cc293a3afbef121378b92a8c53d
Amarnath Revanna <amarnath.revanna@gmail.com>
2014-06-08 20:21:56 +0530

    Exploratory Data Analysis Prjt1: Cleaned source code

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-df = read.table(file="~/../RASPBERRY_PI/Coursera/Data_Science/household_power_consumption.txt", header = T, sep = ";", nrows = 2880, skip = 66636)
+df = read.table(file="~/Coursera/Data_Science/household_power_consumption.txt", header = T, sep = ";", nrows = 2880, skip = 66636)
 png("plot1.png")
 hist(df$X0.326, col="red", , main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts), ylab = Frequency")
 dev.off()
